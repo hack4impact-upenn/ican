9697197173bea2199706df4e242a3ec2d17fca9d
Ali Altaf <alialtaf@seas.upenn.edu>
2014-10-26 17:08:09 -0400

    fixed bug

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from . import db
 
 class Student(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(64), unique=True, index=True)
+    number = db.Column(db.Integer, )
 
     def __repr__(self):
         return '<Student %r>' % self.email
