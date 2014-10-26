ca30db3306cba5c543d8708971b900b418d74013
Ali Altaf <alialtaf@seas.upenn.edu>
2014-10-26 13:39:40 -0400

    Application structure - long overdue

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class Student(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<Student %r>' % self.email
