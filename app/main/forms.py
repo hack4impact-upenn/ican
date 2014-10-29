a5b32a32839986ce3cbdda5b899ecdf8f090c8a3
anniemeng <anniezmeng@gmail.com>
2014-10-29 10:27:10 -0400

    changed routing and added forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,22 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms.validators import Required, EqualTo
 
 
+# @backend TODO check
 class SignupForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     email = StringField('What is your email?', validators=[Required()])
+    number = StringField('What is your phone number?', validators=[Required()])
+    school = SelectField(u'What college are you going attend?',
+     choices=[('temple', 'Temple University'), ('ccp', 'Community College of Philadelphia')], default=None)
+    password = PasswordField('New Password', [Required(), EqualTo('confirm', message='Passwords must match')])
+    confirm  = PasswordField('Repeat Password')
+    submit = SubmitField('Submit')
+
+
+# @backend TODO check
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required()])
+    password = PasswordField('Password', validators=[Required()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -3,24 +3,44 @@ from .. import db
 from ..models import Student
 from ..email import send_email
 from . import main
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 
 
+# @backend TODO check
 @main.route('/')
-@main.route('/index', methods=['GET', 'POST'])
+def landing():
+    return render_template('index.html')
+
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        student = Student.query.filter_by(email=form.email.data).first()
+    form_signup = SignupForm()
+    if form_signup.validate_on_submit():
+        student = Student.query.filter_by(email=form_signup.email.data).first()
         if student is None:
-            student = Student(email=form.email.data, name=form.name.data)
+            student = Student(email=form_signup.email.data, name=form_signup.name.data)
             db.session.add(student)
             return redirect(url_for('.menu'))
         else:
             #throw some error and rerender form
             return redirect(url_for('.menu'))
     return render_template('index.html',
-                           form=form)
+                           form=form_signup)
+
+# @backend TODO check
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = LoginForm()
+    if form_login.validate_on_submit():
+        student = Student.query.filter_by(email=form_login.email.data).first()
+        if student is None:
+            student = Student(email=form_login.email.data, name=form_login.name.data)
+            db.session.add(student)
+            return redirect(url_for('.menu'))
+        else:
+            #throw some error and rerender form
+            return redirect(url_for('.menu'))
+    return render_template('index.html',
+                           form=form_login)
 
 @main.route('/menu')
 def index():
