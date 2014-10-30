385ccb19303b1da6210a65075b8335bbfd4f1a7c
Maya Ebsworth <Mebsworth@levyvlan502.1562.wlan.wireless-resnet.upenn.edu>
2014-10-30 18:05:30 -0400

    updated models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,68 @@
 from . import db
 
+'''
+    Mentor has id, name, email, phone #, and list of students
+'''
+class Mentor(db.Model):
+    __tablename__ = 'mentors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    phone = db.Column(db.Integer, unique=True, index=True)
+
+    students = db.relationship('Student', backref='mentor', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Mentor %r>' % self.email
+
+    # def get_students(self):
+    #     return self.students.all()
+
+    # def add_student(self, s):
+    #     self.students.append(s)
+
+'''
+    Student has id(Integer), name(String), email(String), phone(Integer), 
+    mentor(Mentor), and list of tasks[(Task)]
+'''
 class Student(db.Model):
     __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(64), unique=True, index=True)
-    number = db.Column(db.Integer, )
+    phone = db.Column(db.Integer, unique=True, index=True)
+
+    tasks = db.relationship('Task', backref='student', lazy='dynamic')
+
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'))
 
     def __repr__(self):
         return '<Student %r>' % self.email
+
+'''
+    University has name(String), tasks([(Task)]), students([Student])
+'''
+class University(db.Model):
+    name = db.Column(db.String(64))
+    tasks = db.relationship('Task')
+
+'''
+    Task has description(Text), deadline(DateTime), student(Student), completed(Boolean)
+'''
+class Task(db.Model):
+    description = db.Column(db.Text)
+    deadline = db.Column(db.DateTime)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+
+'''
+    General Task has a description(Text), deadline(DateTime), university(String)
+'''
+class General_Task(db.Model):
+    description = db.Column(db.Text)
+    deadline = db.Column(db.DateTime)
+    university = db.Column(db.String(64))
+    # if university is None, task is general & will be assigned to all students
+    # else task will be specific and assigned to students going to university
+
+
+
