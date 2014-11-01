593e900b8607b8fe4e146336297152daab582435
Ali Altaf <alialtaf@seas.upenn.edu>
2014-11-01 19:30:46 -0400

    added basic login logic

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-# from ..models import Student
+from ..models import User
 from ..email import send_email
+from .forms import LoginForm
 from . import main
 
 @main.route('/')
@@ -9,3 +10,18 @@ from . import main
 def index():
     return render_template('index.html')
 
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            if user.is_role("admin"):
+                return url_for('admin.index')
+            elif user.is_role("student"):
+                return url_form('students.index')
+            else:
+                return url_for('mentors.index')
+        flash('Invalid username or password.')
+    return render_template('login.html', form=form)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db, login_manager
 from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -15,6 +16,17 @@ class User(UserMixin, db.Model):
 
     def is_role(self, role):
         return self.user_role == role
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 @login_manager.user_loader
