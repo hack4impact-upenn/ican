1688a2173e062db681331d16e2b2a34d6012c2c8
Ali Altaf <alialtaf@seas.upenn.edu>
2014-11-04 13:07:07 -0500

    removed TestClient requirement

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,7 +3,6 @@ import unittest
 from flask import url_for
 from app import create_app, db
 from app.models import User
-from flask.ext.testing import assertRedirects
 
 
 class FlaskClientTestCase(unittest.TestCase):
