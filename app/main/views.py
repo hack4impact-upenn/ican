e72ca681fa5e6d87ed3e3dba17d520f49d1a3e2e
Max McCarthy <maxllmcc@icloud.com>
2014-11-08 21:57:56 -0500

    Styled login and signup forms, added navigation components

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import flash, render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
 from ..email import send_email
--- app/students/forms.py
+++ app/students/forms.py
@@ -1,9 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email
 
 
 class SignupForm(Form):
     name = StringField('What is your name?', validators=[Required()])
-    email = StringField('What is your email?', validators=[Required()])
-    submit = SubmitField('Submit')
+    email = StringField('What is your email?', validators=[Required(), Email()])
+    submit = SubmitField('Sign up')
